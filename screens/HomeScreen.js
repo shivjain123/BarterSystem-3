9a74b9e139ec042a0d515140901e605dea9412e5
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-04-01 17:40:09 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, FlatList, KeyboardAvoidingView } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
 import MyHeader from '../components/MyHeader';
 
-import db from '../config';
-import firebase from 'firebase';
+import db from '../config'
 
-export default class HomeScreen extends Component{
-    constructor(){
-        super();
-        this.state={
+export default class HomeScreen extends Component {
+    constructor() {
+        super()
+        this.state = {
             allRequests: []
         }
         this.requestRef = null
@@ -22,9 +21,7 @@ export default class HomeScreen extends Component{
                 snapshot.forEach((doc) => {
                     allRequests.push(doc.data())
                 })
-                this.setState({
-                    allRequests: allRequests
-                });
+                this.setState({ allRequests: allRequests })
             })
     }
 
@@ -56,30 +53,28 @@ export default class HomeScreen extends Component{
         this.requestRef();
     }
 
-    render(){
-        return(
-            <KeyboardAvoidingView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <MyHeader title="Barter App" />
                 <View style={{ flex: 1 }}>
-                    <MyHeader title="Barter App" />
-                        <View style={{ flex: 1 }}>
-                            {
-                                this.state.allRequests.length === 0
-                                    ? (
-                                        <View style={{ flex: 1, fontSize: 20, justifyContent: 'center', alignItems: 'center' }}>
-                                            <Text style={{ fontSize: 20 }}>List of all Barter</Text>
-                                        </View>
-                                    )
-                                    : (
-                                        <FlatList
-                                            keyExtractor={this.keyExtractor}
-                                            data={this.state.allRequests}
-                                            renderItem={this.renderItem}
-                                        />
-                                    )
-                            }
-                    </View>
+                    {
+                        this.state.allRequests.length === 0
+                            ? (
+                                <View style={{ flex: 1, fontSize: 20, justifyContent: 'center', alignItems: 'center' }}>
+                                    <Text style={{ fontSize: 20 }}>List of all Barter</Text>
+                                </View>
+                            )
+                            : (
+                                <FlatList
+                                    keyExtractor={this.keyExtractor}
+                                    data={this.state.allRequests}
+                                    renderItem={this.renderItem}
+                                />
+                            )
+                    }
                 </View>
-            </KeyboardAvoidingView>
+            </View>
         )
     }
 }
